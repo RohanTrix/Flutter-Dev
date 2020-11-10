e760406e69db0e6c25e5c9cd56ef0519323ba9e5
RohanTrix <rohanyeahyeah@gmail.com>
2020-11-10 23:29:52 +0530

    Committed changes

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -5,9 +5,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home : new Login,
     );
   }
+}
